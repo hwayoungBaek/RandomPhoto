bb6497b5e31540daa991a08ad64398da812889cc
hwayoung baek <hybeak0@gmail.com>
2021-05-02 14:38:16 +0900

    Initial Commit

---
+++ RandomPhoto/ViewController.swift
@@ -0,0 +1,73 @@
+//
+//  ViewController.swift
+//  RandomPhoto
+//
+//  Created by 백화영 on 2021/04/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.backgroundColor = .white
+        return imageView
+    }()
+    
+    private let button: UIButton = {
+        let button = UIButton()
+        button.backgroundColor = .white
+        button.setTitle("Random Photo", for: .normal)
+        button.setTitleColor(.black, for: .normal)
+        return button
+    }()
+    
+    let colors: [UIColor] = [
+        .systemPink,
+        .systemBlue,
+        .systemGreen,
+        .systemYellow,
+        .systemPurple,
+        .systemOrange
+    ]
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemPink
+        view.addSubview(imageView)
+        imageView.frame = CGRect(x:0, y:0, width:300, height:300)
+        imageView.center = view.center
+        view.addSubview(button)
+        getRandomPhoto()
+        button.addTarget(self, action: #selector(didTabButton), for: .touchUpInside)
+    }
+    
+    @objc func didTabButton(){
+        getRandomPhoto()
+        
+        view.backgroundColor = colors.randomElement()
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        button.frame = CGRect(
+            x: 30,
+            y: view.frame.size.height-140-view.safeAreaInsets.bottom,
+            width: view.frame.size.width-60,
+            height: 55)
+    }
+    
+    func getRandomPhoto() {
+        let urlString =
+            "https://source.unsplash.com/random/600x600"
+        let url = URL(string: urlString)!
+        guard let data = try? Data(contentsOf: url) else {
+            return
+        }
+        imageView.image = UIImage(data: data)
+    }
+}
+
